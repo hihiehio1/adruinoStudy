2b06e742b7abb84ba323d2071442ec2a5218728d
hihiehio1 <yohan6956@gmail.com>
2022-07-26 17:40:25 +0900

    Arduino Study at 2022-07-26-[time:~0,5]

--- LEDBLINK/LEDBLINK.ino
+++ LEDBLINK/LEDBLINK.ino
@@ -1,22 +1,36 @@
 
 
-
+int i =0;
 void setup() 
 {
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(10,INPUT);
 }
 
 void loop() 
 {
-  digitalWrite(5, HIGH);   
-  delay(200);                       
+  if(digitalRead(10)==HIGH)
+  {
+  digitalWrite(5, HIGH); 
+  digitalWrite(6, HIGH);  
+  delay(1000);                       
   digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);    
-  delay(200); 
   digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);    
-  delay(200); 
-  digitalWrite(7, LOW);                      
+  
+  digitalWrite(6, HIGH); 
+  digitalWrite(7, HIGH);  
+  delay(1000);                       
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+
+  digitalWrite(7, HIGH); 
+  digitalWrite(5, HIGH);  
+  delay(1000);                       
+  digitalWrite(7, LOW);
+  digitalWrite(5, LOW);  
+  }
+  i++;
+ delay(500);                    
 }
